63fa491a894778e1c162ce9b77071c612409dfa9
Piter Capistrano <pitercapistrano@gmail.com>
2024-09-21 21:31:01 -0300

    first commit

---
+++ app/src/main/java/com/pitercapistrano/meuportiflio/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.pitercapistrano.meuportiflio
+
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.pitercapistrano.meuportiflio.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val fotoPerfil = BitmapFactory.decodeResource(resources, R.drawable.foto_piter_perfil)
+        val circulo = RoundedBitmapDrawableFactory.create(resources, fotoPerfil)
+        circulo.isCircular = true
+        binding.fotoPerfil.setImageDrawable(circulo)
+    }
+}
