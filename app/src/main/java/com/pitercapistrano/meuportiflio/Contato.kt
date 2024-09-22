194c4d0fe97ca2e89471c4d61c526a807c1e08eb
Piter Capistrano <pitercapistrano@gmail.com>
2024-09-22 00:43:59 -0300

    Configurando a View da tela de contato

--- app/src/main/java/com/pitercapistrano/meuportiflio/Contato.kt
+++ app/src/main/java/com/pitercapistrano/meuportiflio/Contato.kt
@@ -5,16 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.pitercapistrano.meuportiflio.databinding.ActivityContatoBinding
+import com.pitercapistrano.meuportiflio.databinding.ActivityMainBinding
 
 class Contato : AppCompatActivity() {
+
+    private lateinit var binding: ActivityContatoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_contato)
+        binding = ActivityContatoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val toolbarContato = binding.toolbarContato
+        toolbarContato.setNavigationOnClickListener {
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/pitercapistrano/meuportiflio/Projetos.kt
+++ app/src/main/java/com/pitercapistrano/meuportiflio/Projetos.kt
@@ -5,16 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.pitercapistrano.meuportiflio.databinding.ActivityContatoBinding
+import com.pitercapistrano.meuportiflio.databinding.ActivityProjetosBinding
 
 class Projetos : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProjetosBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_projetos)
+        binding = ActivityProjetosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.toolbarProjetos.setNavigationOnClickListener {
+            finish()
         }
     }
 }
