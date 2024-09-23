808d74a1dc87d319e902bc9742affc30d17287a0
Piter Capistrano <pitercapistrano@gmail.com>
2024-09-23 20:22:35 -0300

    Projeto Concluído!

--- app/src/main/java/com/pitercapistrano/meuportiflio/Projetos.kt
+++ app/src/main/java/com/pitercapistrano/meuportiflio/Projetos.kt
@@ -1,6 +1,7 @@
 package com.pitercapistrano.meuportiflio
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -31,5 +32,54 @@ class Projetos : AppCompatActivity() {
             val intent = Intent(this, DetalhesFPCalculator::class.java)
             startActivity(intent)
         }
+
+        binding.btDtBarberShop.setOnClickListener {
+            val diretorio = "https://github.com/PiterCapistrano/BarberShopApp"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(diretorio))
+            startActivity(intent)
+        }
+
+        binding.btDtDelivery.setOnClickListener {
+            val diretorio = "https://github.com/PiterCapistrano/AppDelivery"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(diretorio))
+            startActivity(intent)
+        }
+
+        binding.btDtFilmes.setOnClickListener {
+            val diretorio = "https://github.com/PiterCapistrano/AppFilmes"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(diretorio))
+            startActivity(intent)
+        }
+
+        binding.btDtBanco.setOnClickListener {
+            val diretorio = "https://github.com/PiterCapistrano/Banco-PC"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(diretorio))
+            startActivity(intent)
+        }
+
+        binding.btDtMusica.setOnClickListener {
+            val diretorio = "https://github.com/PiterCapistrano/PlayMusic"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(diretorio))
+            startActivity(intent)
+        }
+
+        binding.btDtWhatsapp.setOnClickListener {
+            val diretorio = "https://github.com/PiterCapistrano/WhatsApp-List"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(diretorio))
+            startActivity(intent)
+        }
+
+        binding.btDtCredit.setOnClickListener {
+            val diretorio = "https://github.com/PiterCapistrano/credit-application-system"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(diretorio))
+            startActivity(intent)
+        }
+
+        binding.btDtImc.setOnClickListener {
+            val diretorio = "https://github.com/PiterCapistrano/Calculadora-de-IMC"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(diretorio))
+            startActivity(intent)
+        }
+
     }
 }
